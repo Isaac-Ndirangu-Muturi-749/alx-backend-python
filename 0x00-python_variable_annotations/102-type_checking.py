9734c390d46257ad5cf590a9b84c568bf81e4503
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-06-06 10:57:35 +0300

    Update 102-type_checking.py

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Module 102-type_checking
+"""
+
+from typing import Tuple, List
+
+def zoom_array(lst: Tuple[int], factor: int = 2) -> Tuple[int]:
+    """
+    Zooms in on each element in the input tuple by repeating it 'factor' times.
+
+    Args:
+        lst: A tuple of integers.
+        factor: An integer specifying the zoom factor (default is 2).
+
+    Returns:
+        A tuple containing each element from the input tuple repeated 'factor' times.
+    """
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return tuple(zoomed_in)
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)

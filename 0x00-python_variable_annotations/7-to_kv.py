c0fc79c6e0e972668b2e5ff5c080f7f4fdc5d288
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-06-06 15:33:18 +0300

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,6 +4,7 @@ Module 7-to_kv
 """
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple with the first element as the string k

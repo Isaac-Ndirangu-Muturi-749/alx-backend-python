b374a29f79e4434a1bb77151441f9c8822f64cf7
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-06-06 10:57:21 +0300

    Update 100-safe_first_element.py

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module 100-safe_first_element
+"""
+
+from typing import Sequence, Any, Union
+
+def safe_first_element(lst: Sequence) -> Union[Any, None]:
+    """
+    Returns the first element of the list if it exists, otherwise returns None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None

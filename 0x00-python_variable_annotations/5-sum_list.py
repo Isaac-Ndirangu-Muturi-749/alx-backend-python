19c30cf41c6913941af9c26e963f84b790e05b8c
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-06-06 10:57:49 +0300

    Update 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module 5-sum_list
+"""
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sums up the elements of a list of floats and returns the result.
+    """
+    return sum(input_list)

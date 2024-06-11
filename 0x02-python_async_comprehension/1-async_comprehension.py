ef2f313247e46d81162aa640be74e299aed2c0e0
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-06-11 16:19:21 +0300

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,8 +4,8 @@ Module 0-async_comprehension
 """
 
 from typing import List
-from 0-async_generator import async_generator
-
+import asyncio
+async_generator = __import__("0-async_generator").async_generator
 
 async def async_comprehension() -> List[float]:
     """

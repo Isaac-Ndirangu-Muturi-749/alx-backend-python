7b09fa87ffc598b4d06bedd31c8b0a9be788883a
Isaac-Ndirangu-Muturi-749 <ndirangumuturi749@gmail.com>
2024-06-11 16:47:47 +0300

    Create 0-main.py

---
+++ 0x02-python_async_comprehension/0-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def print_yielded_values():
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(print_yielded_values())
+    loop.close()

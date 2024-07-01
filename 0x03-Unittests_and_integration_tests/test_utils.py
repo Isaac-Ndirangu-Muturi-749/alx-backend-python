7a71ff87ea4f5ef6b7044f0965738b258d0409a3
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-07-01 15:41:44 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -31,5 +31,25 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Mapping[str, Any], path: Sequence[str]) -> None:
+        """
+        Test that access_nested_map raises a KeyError for invalid paths.
+
+        Args:
+            nested_map (Mapping): The nested dictionary to access.
+            path (Sequence): The sequence of keys to follow.
+
+        Asserts:
+            That access_nested_map raises a KeyError for invalid paths.
+        """
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), str(path[-1]))
+
+
 if __name__ == '__main__':
     unittest.main()

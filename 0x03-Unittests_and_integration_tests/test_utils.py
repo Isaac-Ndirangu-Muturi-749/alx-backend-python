2c377849b879001379dea32f1e0355f3c684e2f2
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-12-12 18:28:06 +0300

    Revert "Update test_utils.py"
    This reverts commit cd1b8db7c3012fca6c37ab9192ab07186fbc37f7.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,17 +15,24 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-            ) -> None:
-        """Tests `access_nested_map`'s output."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map(self,
+                               nested_map: Mapping[str, Any],
+                               path: Sequence[str],
+                               expected: Any) -> None:
+        """
+        Test that access_nested_map returns the correct value.
 
+        Args:
+            nested_map (Mapping): The nested dictionary to access.
+            path (Sequence): The sequence of keys to follow.
+            expected (Any): The expected value.
+
+        Asserts:
+            That the value returned by access_nested_map is equal to expected.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",)),

8cb1ec2f3b9243e3de0f206298bf266e03b094f0
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-12-12 17:36:24 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,6 @@
 """Module test_utils
 """
 
-
 import unittest
 from typing import Mapping, Any, Sequence
 from parameterized import parameterized
@@ -11,15 +10,28 @@ from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Unit tests for the access_nested_map function"""
+    """Test case for the access_nested_map function."""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, name, nested_map, path, expected):
-        """Test access_nested_map with various inputs."""
+    def test_access_nested_map(self,
+                               nested_map: Mapping[str, Any],
+                               path: Sequence[str],
+                               expected: Any) -> None:
+        """
+        Test that access_nested_map returns the correct value.
+
+        Args:
+            nested_map (Mapping): The nested dictionary to access.
+            path (Sequence): The sequence of keys to follow.
+            expected (Any): The expected value.
+
+        Asserts:
+            That the value returned by access_nested_map is equal to expected.
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([

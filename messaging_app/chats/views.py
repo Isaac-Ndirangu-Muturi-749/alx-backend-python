77be49fc7ec02f93e4619594989f3269800e9a97
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2024-12-23 19:43:27 +0300

    Add user authentication using JWT

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -5,12 +5,16 @@ from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from django.shortcuts import get_object_or_404
 
+from .permissions import IsOwner
+from rest_framework.permissions import IsAuthenticated
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['participants__email', 'participants__first_name', 'participants__last_name']
+    permission_classes = [IsAuthenticated, IsOwner]  # Ensure only the owner can access their conversation
 
     @action(detail=False, methods=['post'], url_path='create', url_name='create_conversation')
     def create_conversation(self, request):
@@ -28,12 +32,17 @@ class ConversationViewSet(viewsets.ModelViewSet):
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
+    def get_queryset(self):
+        user = self.request.user
+        return Conversation.objects.filter(participants=user)
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['message_body', 'sender__email']
+    permission_classes = [IsAuthenticated, IsOwner]  # Ensure only the owner can access their messages
 
     def create(self, request, *args, **kwargs):
         conversation_id = request.data.get('conversation_id')
@@ -50,3 +59,7 @@ class MessageViewSet(viewsets.ModelViewSet):
         message = Message.objects.create(conversation=conversation, sender=sender, message_body=message_body)
         serializer = self.get_serializer(message)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Message.objects.filter(sender=user)
